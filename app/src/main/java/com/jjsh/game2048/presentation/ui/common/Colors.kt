a306e707a93b8ae1aa00a124a7b73ba812801f80
skipancho <ctg1770a@gmail.com>
2023-02-12 18:45:47 +0900

    [FEATURE] #1 - Colors 추가

---
+++ app/src/main/java/com/jjsh/game2048/presentation/ui/common/Colors.kt
@@ -0,0 +1,243 @@
+package com.jjsh.game2048.presentation.ui.common
+
+object Colors {
+    const val IDX_50 = 0
+    const val IDX_100 = 1
+    const val IDX_200 = 2
+    const val IDX_300 = 3
+    const val IDX_400 = 4
+    const val IDX_500 = 5
+    const val IDX_600 = 6
+    const val IDX_700 = 7
+    const val IDX_800 = 8
+    const val IDX_900 = 9
+
+    val RED = intArrayOf(
+        -0x1412,
+        -0x322e,
+        -0x106566,
+        -0x1a8c8d,
+        -0x10acb0,
+        -0xbbcca,
+        -0x1ac6cb,
+        -0x2cd0d1,
+        -0x39d7d8,
+        -0x48e3e4
+    )
+    val DEEP_PURPLE = intArrayOf(
+        -0x12180a,
+        -0x2e3b17,
+        -0x4c6225,
+        -0x6a8a33,
+        -0x81a83e,
+        -0x98c549,
+        -0xa1ca4f,
+        -0xaed258,
+        -0xbad860,
+        -0xcee46e
+    )
+    val LIGHT_BLUE = intArrayOf(
+        -0x1e0a02,
+        -0x4c1a04,
+        -0x7e2b06,
+        -0xb03c09,
+        -0xd6490a,
+        -0xfc560c,
+        -0xfc641b,
+        -0xfd772f,
+        -0xfd8843,
+        -0xfea865
+    )
+    val GREEN = intArrayOf(
+        -0x170a17,
+        -0x371937,
+        -0x5a2959,
+        -0x7e387c,
+        -0x994496,
+        -0xb350b0,
+        -0xbc5fb9,
+        -0xc771c4,
+        -0xd182ce,
+        -0xe4a1e0
+    )
+    val YELLOW = intArrayOf(
+        -0x219,
+        -0x63c,
+        -0xa63,
+        -0xe8a,
+        -0x11a8,
+        -0x14c5,
+        -0x227cb,
+        -0x43fd3,
+        -0x657db,
+        -0xa80e9
+    )
+    val DEEP_ORANGE = intArrayOf(
+        -0x41619,
+        -0x3344,
+        -0x546f,
+        -0x759b,
+        -0x8fbd,
+        -0xa8de,
+        -0xbaee2,
+        -0x19b5e7,
+        -0x27bceb,
+        -0x40c9f4
+    )
+    val BLUE_GREY = intArrayOf(
+        -0x13100f,
+        -0x302724,
+        -0x4f413b,
+        -0x6f5b52,
+        -0x876f64,
+        -0x9f8275,
+        -0xab9186,
+        -0xbaa59c,
+        -0xc8b8b1,
+        -0xd9cdc8
+    )
+    val PINK = intArrayOf(
+        -0x31b14,
+        -0x74430,
+        -0xb704f,
+        -0xf9d6e,
+        -0x13bf86,
+        -0x16e19d,
+        -0x27e4a0,
+        -0x3de7a5,
+        -0x52eba9,
+        -0x77f1b1
+    )
+    val INDIGO = intArrayOf(
+        -0x17150a,
+        -0x3a3517,
+        -0x605726,
+        -0x867935,
+        -0xa39440,
+        -0xc0ae4b,
+        -0xc6b655,
+        -0xcfc061,
+        -0xd7ca6d,
+        -0xe5dc82
+    )
+    val CYAN = intArrayOf(
+        -0x1f0806,
+        -0x4d140e,
+        -0x7f2116,
+        -0xb22f1f,
+        -0xd93926,
+        -0xff432c,
+        -0xff533f,
+        -0xff6859,
+        -0xff7c71,
+        -0xff9f9c
+    )
+    val LIGHT_GREEN = intArrayOf(
+        -0xe0717,
+        -0x231238,
+        -0x3a1e5b,
+        -0x512a7f,
+        -0x63339b,
+        -0x743cb6,
+        -0x834cbe,
+        -0x9760c8,
+        -0xaa74d1,
+        -0xcc96e2
+    )
+    val AMBER = intArrayOf(
+        -0x71f,
+        -0x134d,
+        -0x1f7e,
+        -0x2ab1,
+        -0x35d8,
+        -0x3ef9,
+        -0x4d00,
+        -0x6000,
+        -0x7100,
+        -0x9100
+    )
+    val BROWN = intArrayOf(
+        -0x101417,
+        -0x283338,
+        -0x43555c,
+        -0x5e7781,
+        -0x72919d,
+        -0x86aab8,
+        -0x92b3bf,
+        -0xa2bfc9,
+        -0xb1cbd2,
+        -0xc1d8dd
+    )
+    val PURPLE = intArrayOf(
+        -0xc1a0b,
+        -0x1e4119,
+        -0x316c28,
+        -0x459738,
+        -0x54b844,
+        -0x63d850,
+        -0x71db56,
+        -0x84e05e,
+        -0x95e466,
+        -0xb5eb74
+    )
+    val BLUE = intArrayOf(
+        -0x1c0d03,
+        -0x442105,
+        -0x6f3507,
+        -0x9b4a0a,
+        -0xbd5a0b,
+        -0xde690d,
+        -0xe1771b,
+        -0xe6892e,
+        -0xea9a40,
+        -0xf2b85f
+    )
+    val TEAL = intArrayOf(
+        -0x1f0d0f,
+        -0x4d2025,
+        -0x7f343c,
+        -0xb24954,
+        -0xd95966,
+        -0xff6978,
+        -0xff7685,
+        -0xff8695,
+        -0xff96a4,
+        -0xffb2c0
+    )
+    val LIME = intArrayOf(
+        -0x60419,
+        -0xf0b3d,
+        -0x191164,
+        -0x23188b,
+        -0x2b1ea9,
+        -0x3223c7,
+        -0x3f35cd,
+        -0x504bd5,
+        -0x6162dc,
+        -0x7d88e9
+    )
+    val ORANGE = intArrayOf(
+        -0xc20,
+        -0x1f4e,
+        -0x3380,
+        -0x48b3,
+        -0x58da,
+        -0x6800,
+        -0x47400,
+        -0xa8400,
+        -0x109400,
+        -0x19af00
+    )
+    val GREY = intArrayOf(
+        -0x50506,
+        -0xa0a0b,
+        -0x111112,
+        -0x1f1f20,
+        -0x424243,
+        -0x616162,
+        -0x8a8a8b,
+        -0x9e9e9f,
+        -0xbdbdbe,
+        -0xdededf
+    )
+}
