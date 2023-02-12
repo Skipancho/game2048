8636aabd3b3af444e4ee84b9bedfaeb35cd7a13d
skipancho <ctg1770a@gmail.com>
2023-02-12 21:47:39 +0900

    [FEATURE] #1 - 블록 이동 이벤트 구현

--- app/src/main/java/com/jjsh/game2048/presentation/ui/view/GameView.kt
+++ app/src/main/java/com/jjsh/game2048/presentation/ui/view/GameView.kt
@@ -5,8 +5,12 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import com.jjsh.game2048.presentation.ui.common.Colors
+import timber.log.Timber
+import java.util.*
+import kotlin.math.abs
 
 class GameView(
     context: Context,
@@ -130,6 +134,175 @@ class GameView(
                 }
             }
         }
+    }
+
+    private var oldX = 0f
+    private var oldY = 0f
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                oldX = event.x
+                oldY = event.y
+            }
+            MotionEvent.ACTION_UP -> {
+                val moveX = oldX - event.x
+                val moveY = oldY - event.y
+                if (moveX == 0f && moveY == 0f) return false
+                if (abs(moveX) > abs(moveY)){
+                    if (moveX > 0){
+                        moveLeftAction()
+                    }else if (moveX < 0) {
+                        moveRightAction()
+                    }
+                }else if (abs(moveX) < abs(moveY)) {
+                    if (moveY > 0) {
+                        moveUpAction()
+                    }else if (moveY < 0){
+                        moveDownAction()
+                    }
+                }
+            }
+        }
+
+        performClick()
+        return true
+    }
+
+    private fun moveUpAction() {
+        Timber.e("moveUp")
+        for (row in gameNumbers) {
+            val stack = Stack<Int>()
+
+            for (idx in row.indices){
+                if (stack.isNotEmpty() && stack.peek() == row[idx]){
+                    stack.add(stack.pop() + row[idx])
+                    stack.add(0)
+                }else {
+                    stack.add(row[idx])
+                }
+            }
+
+            val newRow = mutableListOf<Int>()
+            while (stack.isNotEmpty()) {
+                val cur = stack.pop()
+                if (cur > 0) {
+                    newRow.add(0,cur)
+                }
+            }
+
+            var newRowIdx = 0
+            for (n in newRow){
+                row[newRowIdx++] = n
+            }
+            for (i in newRowIdx .. row.lastIndex) {
+                row[i] = 0
+            }
+        }
+        createNewNumber()
+    }
+
+    private fun moveDownAction() {
+        Timber.e("moveDown")
+        for (row in gameNumbers ) {
+            val stack = Stack<Int>()
+
+            for (idx in row.lastIndex downTo 0){
+                if (stack.isNotEmpty() && stack.peek() == row[idx]){
+                    stack.add(stack.pop() + row[idx])
+                    stack.add(0)
+                }else {
+                    stack.add(row[idx])
+                }
+            }
+
+            val newRow = mutableListOf<Int>()
+            while (stack.isNotEmpty()) {
+                val cur = stack.pop()
+                if (cur > 0) {
+                    newRow.add(0,cur)
+                }
+            }
+
+            var newRowIdx = row.lastIndex
+            for (n in newRow){
+                row[newRowIdx--] = n
+            }
+
+            for (i in newRowIdx downTo 0) {
+                row[i] = 0
+            }
+        }
+        createNewNumber()
+    }
+
+    private fun moveLeftAction() {
+        Timber.e("moveLeft")
+        for (c in gameNumbers[0].indices){
+            val stack = Stack<Int>()
+            for (r in gameNumbers.indices){
+                if (stack.isNotEmpty() && stack.peek() == gameNumbers[r][c]){
+                    stack.add(stack.pop() + gameNumbers[r][c])
+                    stack.add(0)
+                }else {
+                    stack.add(gameNumbers[r][c])
+                }
+            }
+            val newCol = mutableListOf<Int>()
+            while (stack.isNotEmpty()) {
+                val cur = stack.pop()
+                if (cur > 0) {
+                    newCol.add(0,cur)
+                }
+            }
+
+            var newColIdx = 0
+            for (n in newCol){
+                gameNumbers[newColIdx++][c] = n
+            }
+
+            for (i in newColIdx .. gameNumbers[c].lastIndex) {
+                gameNumbers[i][c] = 0
+            }
+        }
+        createNewNumber()
+    }
+
+    private fun moveRightAction() {
+        Timber.e("moveRight")
+        for (c in gameNumbers[0].indices){
+            val stack = Stack<Int>()
+            for (r in gameNumbers.lastIndex downTo 0){
+                if (stack.isNotEmpty() && stack.peek() == gameNumbers[r][c]){
+                    stack.add(stack.pop() + gameNumbers[r][c])
+                    stack.add(0)
+                }else {
+                    stack.add(gameNumbers[r][c])
+                }
+            }
+            val newCol = mutableListOf<Int>()
+            while (stack.isNotEmpty()) {
+                val cur = stack.pop()
+                if (cur > 0) {
+                    newCol.add(0,cur)
+                }
+            }
+
+            var newColIdx = gameNumbers[c].lastIndex
+            for (n in newCol){
+                gameNumbers[newColIdx--][c] = n
+            }
+
+            for (i in newColIdx downTo 0) {
+                gameNumbers[i][c] = 0
+            }
+        }
+        createNewNumber()
+    }
+
+    override fun performClick(): Boolean {
+        super.performClick()
+        return true
     }
 
     companion object {
